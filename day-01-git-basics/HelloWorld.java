71ecefbfc405b60347d85d0a5957fea8f8e82121
Chinmay <chinmaythecoder7@gmail.com>
2026-06-06 00:50:11 +0530

    Day 1: Add HelloWorld with basic Java variables

---
+++ day-01-git-basics/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+* Day 1 - Development Journey
+* Revisiting Java fundamentals before DSA practice.
+
+*/
+
+public class HelloWorld{
+    public static void main(String[] args){
+            String developerName = "Chinmay";
+            String goal = "Land first internship";
+            int targetDays = 30;
+
+
+        System.out.println("Developer: " + developerName);
+        System.out.println("Goal: " + goal);
+        System.out.println("Days remaining: " + targetDays);
+    }
+}
